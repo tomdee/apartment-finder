766c393e8385d8859936c174d2cd1ead99184e0f
nat45928 <nal2p7@mst.edu>
2016-12-30 19:47:06 -0600

    More matching boxes

--- settings.py
+++ settings.py
@@ -24,9 +24,77 @@ AREAS = ["wst", "sfv", "lac", "sgv", "lgb", "ant"]
 # name.  If no match, the neighborhood field, which is a string, will be checked to see if it matches
 # anything in NEIGHBORHOODS.
 BOXES = {
-    "la": [
-        [33.788279, -118.547974],
-        [34.066312, -118.155289]
+    # "la": [
+    #   [33.788279, -118.547974],
+    #   [34.066312, -118.155289]
+    # ],
+    "Marina Del Rey": [
+        [33.959878, -118.47888],
+        [33.992192, -118.430214]
+    ],
+    "Venice": [
+        [33.987068, -118.501453],
+        [34.007206, -118.445406]
+    ],
+    "Santa Monica": [
+        [34.006317, -118.51686],
+        [34.048535, -118.432789]
+    ],
+    "Culver City": [
+        [33.978812, -118.43914],
+        [34.049299, -118.348696]
+    ],
+    "Playa Vista": [
+        [33.943974, -118.458785],
+        [33.977816, -118.397598]
+    ],
+    "Inglewood": [
+        [33.927978, -118.405876],
+        [34.003293, -118.278637]
+    ],
+    "DTLA": [
+        [34.00742, -118.282585],
+        [34.081633, -118.210521]
+    ],
+    "Hollywood": [
+        [34.029918, -118.470538],
+        [34.104112, -118.275032]
+    ],
+    "El Segundo": [
+        [33.900199, -118.442526],
+        [33.934735, -118.368931]
+    ],
+    "Hawthorne": [
+        [33.898886, -118.37071],
+        [33.933422, -118.257694]
+    ],
+    "Manhattan Beach": [
+        [33.872839, -118.423722],
+        [33.90251, -118.370476],
+    ],
+    "Gardena": [
+        [33.871968, -118.37135],
+        [33.90164, -118.263531]
+    ],
+    "Hermosa Beach": [
+        [33.847449, -118.415116],
+        [33.87713, -118.250656]
+    ],
+    "Redondo Beach": [
+        [33.786236, -118.412189],
+        [33.851472, -118.35331]
+    ],
+    "Torrance": [
+        [33.787151, -118.354755],
+        [33.852387, -118.279187]
+    ],
+    "Carson": [
+        [33.786885, -118.29679],
+        [33.871122, -118.202307]
+    ],
+    "Long Beach": [
+        [33.730575, -118.2148],
+        [33.873782, -118.118869]
     ]
 }
 
@@ -34,7 +102,9 @@ BOXES = {
 # one of the boxes you defined, it will be checked to see if the neighborhood name it was listed under matches one
 # of these.  This is less accurate than the boxes, because it relies on the owner to set the right neighborhood,
 # but it also catches listings that don't have coordinates (many listings are missing this info).
-NEIGHBORHOODS = []
+NEIGHBORHOODS = ["marina del rey", "venice", "santa monica", "culver city", "mar vista", "playa vista",
+                 "inglewood", "la", "downtown", "el segundo", "gardena", "hawthorne", "manhattan",
+                 "manhattan beach", "hermosa", "hermosa beach", "torrance", "long beach", "signal hill"]
 
 ## Transit preferences
 
